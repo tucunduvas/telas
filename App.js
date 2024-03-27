1bb9048e47f05687f4f46cf6d40f4885efec104b
tucunduvas <gabriela.tucunduva@estudante.ifms.edu.br>
2024-03-27 12:24:43 -0400

    CLASSES

--- App.js
+++ App.js
@@ -46,7 +46,7 @@ class App extends Component{
         <View>
             <Text> Olá Mundo! </Text>
             <Text> Exemplo! </Text>
-            <text style={{color: 'blue', fontSize:30, margin:15}}> meu exemplo! </text>
+            <text style={{color: 'blue', fontSize:30, margin:15}}> meu exemplo! </text>  {/* nao precisa por px na formatacao, se por da erro*/}
 
             <Image source={{URL: 'url aqui'}}style={{width:300, heigth: 300}}/>
             <Text> {curso}</Text>
@@ -55,12 +55,35 @@ class App extends Component{
 }
 
 }
-
+ //extends pega todas as caracteristicas da classe component
 class App extends Component{
-    <Image source={{'url: coloca a url aqui'}} style={{width:300, height:300}}/>
+    <Image source={{'url: coloca a url aqui'}} style={{width:300, height:300}}/> 
+    {/* uri é o caminho da iamgem, se for por o libnk da internet tem que por com https */}
     {/* Image é obrigado a informar a largura e altura */}
     <Text> {Curso}</Text>
 
     {/* para chamar o componente */}
-    {/* <MinhaImagem> }
-}
+    {/* <MinhaImagem/>  */}
+    {/* <MinhaImagem largura={400} altura{400} nome= "Imagem 1"/> */} {/*<- EXEMPLO DE PROPRIEDADE */}
+       
+    </View> 
+
+};
+
+export default App;
+
+    class MinhaImagem extends Component{
+        render(){
+            let img = 'url da imagem';
+            return(
+                <View>
+                    {/* <Image source={{url: img}} style={{width:300, height:300}}/>  COMENTEI PQ É IGUAL A DE BAIXO*/}
+                    <Image source={{uri: img}} style={{width: this.props.largura, height:this.props.altura}}> </Image> 
+{/* PASSA PROPRIEDADE DE UMA CLASSE PARA OUTRA                             */}
+                    <Text> {this.props.nome}</Text>
+                </View>
+            );
+        }
+    }
+
+    
